8b0252e895c2d98005341fff60a76fc770fc0cba
sunkang <sunkang2101024@foxmail.com>
2022-06-21 21:43:45 +0800

    test: add an example of JSON

--- examples/json-object/main.cpp
+++ examples/json-object/main.cpp
@@ -7,13 +7,23 @@ using std::endl;
 
 int main()
 {
-    JsonObject rootObject;
-    rootObject.insert("id", 1024);
-    JsonArray arry({1, 2, 3, 4, 5, 6, 7, 8, 9, 10});
-    arry.append(1024);
-    rootObject.insert("arry_data", arry);
+    {
+        JsonObject rootObject;
+        rootObject.insert("id", 1024);
+        JsonArray arry({1, 2, 3, 4, 5, 6, 7, 8, 9, 10});
+        arry.append(1024);
+        rootObject["arry_data"] = arry;
 
-    cout << JsonDocument(rootObject).toJson(JsonDocument::Compact) << endl;
-    cout << JsonDocument(rootObject).toJson(JsonDocument::Indented) << endl;
+        cout << JsonDocument(rootObject).toJson() << endl;
+    }
+
+    {
+        JsonObject rootObject;
+        rootObject["objectInfo"]["test_1"]["info_1"] = false;
+        rootObject["objectInfo"]["test_1"]["info_2"] = JsonValue();
+        rootObject["objectInfo"]["test_1"]["info_3"]["info_4"] = JsonArray({1,2,3,4,5,6,7,8,9,10});
+        cout << JsonDocument(rootObject).toJson() << endl;
+    }
+
     return 0;
 }
